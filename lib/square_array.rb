e1b3f930b82d4e68b27812e1710c749af8dbcb30
SOLOMON YISMAW <fantahunsolomon@gmail.com>
2020-04-18 16:47:43 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,16 @@
-def square_array(array)
-  # your code here
+#def square_array(array)
+#  array.each do |index|
+#    index ** 2
+#end
+
+def square_array(ary)
+  a = Array.new(ary.length)
+  ary.each_index { |i| a[i] = ary[i] * ary[i] }
+  a
 end
+
+#def square_array(array)
+#  new_array = []
+#  array.each { |array| new_array << array ** 2 }
+#  new_array
+#end
