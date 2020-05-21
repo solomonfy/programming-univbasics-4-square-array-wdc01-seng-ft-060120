1956fc9c765fa05d47649eabb75e8b696338d921
solomonfy <62584475+solomonfy@users.noreply.github.com>
2020-05-21 12:26:47 -0400

    Update square_array.rb

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,16 +1,9 @@
-#def square_array(array)
-#  array.each do |index|
-#    index ** 2
-#end
-
-def square_array(ary)
-  a = Array.new(ary.length)
-  ary.each_index { |i| a[i] = ary[i] * ary[i] }
-  a
+def square_array(numbers)
+  sq_numbers = []   # or sq_numbers = Array.new
+  count = 0
+while count < numbers.length do
+  sq_numbers << number[count]**2
+  count += 1
 end
-
-#def square_array(array)
-#  new_array = []
-#  array.each { |array| new_array << array ** 2 }
-#  new_array
-#end
+sq_numbers
+end
